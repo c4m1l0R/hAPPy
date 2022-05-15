ef106739e21bb99a9074391897adf821a0d4ba73
Camilo <92024347+c4m1l0R@users.noreply.github.com>
2022-05-16 01:40:16 +0200

    primer usuario añadido a Firebase

--- app/src/main/java/com/example/happy/LogActivity.java
+++ app/src/main/java/com/example/happy/LogActivity.java
@@ -6,25 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogActivity extends AppCompatActivity {
 
-    Button registro;
-    Button entrar;
+    private Button registro;
+    private Button entrar;
+    private EditText email;
+    private EditText contraseña;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //SPLASH
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        //CARGAR INTERFAZ
         setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 
+        //FIREBASE
+        mAuth = FirebaseAuth.getInstance();
+
+
+        //BOTONES Y CUADROS DE TEXTOS
         registro = (Button) findViewById(R.id.log_registro);
         entrar = (Button) findViewById(R.id.entrar);
+        email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        contraseña = (EditText) findViewById(R.id.editTextTextPassword);
 
 
         registro.setOnClickListener(new View.OnClickListener() {
@@ -45,4 +62,5 @@ public class LogActivity extends AppCompatActivity {
             }
         });
     }
+
 }
--- app/src/main/java/com/example/happy/RegistroActivity.java
+++ app/src/main/java/com/example/happy/RegistroActivity.java
@@ -1,40 +1,67 @@
 package com.example.happy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.PopupWindow;
+import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegistroActivity extends AppCompatActivity {
 
-    //botónes
-    Button registrar;
+    //RECURSOS
+    private EditText nombre;
+    private EditText apellido1;
+    private EditText apellido2;
+    private EditText email;
+    private EditText pwd;
+    private EditText editTextBirthDate;
+    private Button registrar;
+    private ProgressDialog barraDeProgreso;
+
+    //FIREBASE
+    private FirebaseAuth mAuth;
 
 
-    //editTexts
-    EditText editTextBirthDate;
 
-
-    //código
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        registrar = (Button) findViewById(R.id.registrar);
+        //FIREBASE
+        mAuth = FirebaseAuth.getInstance();
 
+        //RECURSOS
+        nombre = (EditText) findViewById(R.id.editTextTextName);
+        apellido1 = (EditText) findViewById(R.id.editTextTextSurname1);
+        apellido2 = (EditText) findViewById(R.id.editTextTextSurname2);
+        email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        pwd = (EditText) findViewById(R.id.editTextTextPassword);
         editTextBirthDate = (EditText) findViewById(R.id.editTextBirthDate);
+        registrar = (Button) findViewById(R.id.registrar);
+        barraDeProgreso = new ProgressDialog(this);
+
 
 
         editTextBirthDate.setOnClickListener(new View.OnClickListener() {
@@ -49,12 +76,13 @@ public class RegistroActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                mostrarDialogoError();
+                registrarUsuario();
+
             }
         });
     }
 
-    private  void showDatePickerDialog(){
+    private void showDatePickerDialog(){
 
         DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
 
@@ -70,27 +98,73 @@ public class RegistroActivity extends AppCompatActivity {
 
     }
 
-    private void mostrarDialogoError(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(RegistroActivity.this);
+    private void registrarUsuario(){
 
-        LayoutInflater inflater = getLayoutInflater();
+        String rNombre = nombre.getText().toString().trim();
+        String rapellido1 = apellido1.getText().toString().trim();
+        String rapellido2 = apellido2.getText().toString().trim();
+        String rEmail = email.getText().toString().trim();
+        String rPwd = pwd.getText().toString().trim();
+        String rBirthDay = editTextBirthDate.getText().toString().trim();
 
-        View view = inflater.inflate(R.layout.dialogo_error_registro, null);
+        //BERIFICAMOS QUE LOS CAMPOS ESTEN COMPLETADOS
+        if(TextUtils.isEmpty(rNombre)){
 
-        builder.setView(view);
+            Toast.makeText(this, "Se debe ingresar un nombre", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        final AlertDialog dialog= builder.create();
-        dialog.show();
+        if(TextUtils.isEmpty(rapellido1)){
+
+            Toast.makeText(this, "Se debe ingresar un Primer Apellido", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(rapellido2)){
+
+            Toast.makeText(this, "Se debe ingresar un Segundo Apellido", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(rEmail)){
+
+            Toast.makeText(this, "Se debe ingresar un Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(rPwd)){
+
+            Toast.makeText(this, "Se debe ingresar una contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(rBirthDay)){
+
+            Toast.makeText(this, "Se debe ingresar una fecha de cumpleaños", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        barraDeProgreso.setMessage("Realizando registro en linea...");
+        barraDeProgreso.show();
+
+        //CREAMOS USUARIO EN FIREBASE
+        mAuth.createUserWithEmailAndPassword(rEmail, rPwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(RegistroActivity.this, "Se ha registrado el usuario", Toast.LENGTH_SHORT).show();
 
 
-        Button aceptarError = (Button) view.findViewById(R.id.button_aceptar_error);
-        aceptarError.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegistroActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
 
-                dialog.dismiss();
-            }
-        });
+                        }
 
+                        barraDeProgreso.dismiss();
+                    }
+                });
     }
+
 }
